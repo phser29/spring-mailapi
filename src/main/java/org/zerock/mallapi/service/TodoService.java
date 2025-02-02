76ba7bbf91bfa524bd7ade2c7038a92daa7fee39
phs28 <104198099+phser29@users.noreply.github.com>
2025-02-02 17:17:23 +0900

    commit

--- src/main/java/org/zerock/mallapi/service/TodoService.java
+++ src/main/java/org/zerock/mallapi/service/TodoService.java
@@ -1,7 +1,14 @@
 package org.zerock.mallapi.service;
 
+import org.zerock.mallapi.domain.PageRequestDTO;
+import org.zerock.mallapi.domain.PageResponseDTO;
 import org.zerock.mallapi.domain.TodoDTO;
 
 public interface TodoService {
   Long register(TodoDTO todoDTO);
+  TodoDTO get(Long tno);
+  void modify(TodoDTO todoDTO);
+  void remove(Long tno);
+
+  PageResponseDTO<TodoDTO> list(PageRequestDTO pageRequestDTO);
 }
